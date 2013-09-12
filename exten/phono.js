d3118e070882cdda7c55c75935eaf76c734d156f
Tim Panton <tim@phonefromhere.com>
2013-09-12 17:51:25 +0100

    First working integration with atlassian. Loads of missing features.

--- exten/phono.js
+++ exten/phono.js
@@ -1,7 +1,8 @@
 var apiKey="C17D167F-09C6-4E4C-A3DD-2025D48BA243";
-var phone = {onIncomingCall:function (evt) { call=evt.call;call.answer()}};
+var phone = {onIncomingCall:function (evt) { call=evt.call;}};
 var audio = {media:{audio:true,video:false}};
-var connectionUrl = "https://app.v1-1.phono.com/http-bind";
+var connectionUrl = "https://jiraconf.westhawk.co.uk:8443/http-bind";
+var call;
 var phono = $.phono(
     {apiKey:apiKey,
      connectionUrl:connectionUrl, 
@@ -10,16 +11,6 @@ var phono = $.phono(
      onReady: function() {
         var name = $('a#header-details-user-fullname').attr('data-username');
         console.log("Phono is ready for "+name);
-        $.ajax({url:"http://gont.westhawk.co.uk/bm2012/setJid.groovy?name="+name+"&jid=xmpp:"+phono.sessionId});
+        $.ajax({url:"https://jiraconf.westhawk.co.uk:8443/prov/setJid.groovy?name="+name+"&jid=xmpp:"+phono.sessionId});
      }
-    });
-window.addEventListener('message', function(event) {
-  var command = event.data.command;
-  var name = event.data.address || 'app:9996160714';
-  console.log("got message: "+command+" address "+name);
-  switch(command) {
-    case 'dial':
-      phono.phone.dial(name);
-      break;
-  }
 });
--- exten/popup.js
+++ exten/popup.js
@@ -1,7 +1,7 @@
 var phonoUsers = {
   requestList: function() {
     var req = new XMLHttpRequest();
-    req.open("GET", "http://gont.westhawk.co.uk/bm2012/jsonusers.groovy", true);
+    req.open("GET", "https://jiraconf.westhawk.co.uk:8443/prov/jsonusers.groovy", true);
     req.onload = this.showUsers_.bind(this);
     req.send(null);
   },
@@ -10,15 +10,16 @@ var phonoUsers = {
     var resp = e.target.responseText;
     var users = JSON.parse(resp);
     console.log("resp = "+resp);
+    var b;
     for (var i = 0; i < users.length; i++) {
       console.log("user ["+i+"] = "+users[i].user);
+      b = document.createElement('br');
       var a = document.createElement('a');
-      var b = document.createElement('br');
       a.onclick = function(){
         addr = this.href;
         console.log("asking Phono to dial"+addr);
         chrome.tabs.executeScript(null,
-        {code:"phono.phone.dial('" + addr + "')"});
+        {code:"call = phono.phone.dial('" + addr + "')"});
         window.close();
       };
       a.href=users[i].address;
@@ -26,6 +27,38 @@ var phonoUsers = {
       document.body.appendChild(a);
       document.body.appendChild(b);
     }
+    b = document.createElement('br');
+    document.body.appendChild(b);
+    var buttons=["answer","hangup","mute","unmute"];
+    for (var i = 0; i < buttons.length; i++) {
+      var a = document.createElement('a');
+      b = document.createElement('br');
+      a.onclick = function(){
+        var act = this.innerHTML;
+        console.log("asking Phono to "+act);
+        chrome.tabs.executeScript(null,
+        {code:"call."+act+"()"});
+        window.close();
+      };
+      a.innerHTML = buttons[i];
+      document.body.appendChild(a);
+      document.body.appendChild(b);
+    }
+    b = document.createElement('br');
+    document.body.appendChild(b);
+    for (var i = 0; i < 10; i++) {
+      var a = document.createElement('a');
+      a.onclick = function(){
+        var act = this.innerHTML;
+        console.log("asking Phono to "+act);
+        chrome.tabs.executeScript(null,
+        {code:"call.digit('"+act.trim()+"')"});
+      };
+      a.innerHTML = " "+i;
+      document.body.appendChild(a);
+    }
+    b = document.createElement('br');
+    document.body.appendChild(b);
   }
 };
 
